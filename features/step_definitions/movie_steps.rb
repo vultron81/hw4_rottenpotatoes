1e4b4ae054eb45cf62e4456b0452441674c1ab98
Ben Williams <vultron81@gmail.com>
2012-03-22 14:34:19 -0700

    Add cucumber file for director and make first scenario green

---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,41 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
+  end
+end
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  assert page.body =~ /.*#{e1}.*#{e2}.*/m, page.body
+end
+
+Then /the director of "(.*)" should be "(.*)"/ do |title, director|
+  assert Movie.find_by_title(title).director == director
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(',').each do |rating| 
+    step %Q{I #{uncheck}check "ratings_#{rating.strip}"} 
+  end
+end
+
+Then /I should see all of the movies/ do 
+  # Count rows but subtract one for the header and the number in the DB and compare
+  assert (page.all('tr').count-1) == Movie.count
+end
+
